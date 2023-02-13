e2fb3c876d0aa469b9fae75a6cba14b6bee22033
Emma Plessinger <plessinger.em@gmail.com>
2023-02-12 23:05:25 -0700

    Add project files.

---
+++ Mission06_plessem/Controllers/HomeController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Mission06_plessem.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission06_plessem.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private MovieDatabaseContext _MovieContext { get; set; }
+
+        //constructor
+        public HomeController(ILogger<HomeController> logger, MovieDatabaseContext someName)
+        {
+            _logger = logger;
+            _MovieContext = someName;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult MovieForm ()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult MovieForm(ApplicationResponse ar)
+        {
+            //Saving changes to database
+            _MovieContext.Add(ar);
+            _MovieContext.SaveChanges();
+
+            return View("Confirmation", ar);
+        }
+
+        public IActionResult MyPodcasts()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Mission06_plessem/Models/MovieDatabaseContext.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission06_plessem.Models
+{
+    public class MovieDatabaseContext : DbContext
+    {
+        //Constructor
+        public MovieDatabaseContext (DbContextOptions<MovieDatabaseContext> options) : base (options)
+        {
+
+        }
+
+        public DbSet<ApplicationResponse> Responses { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder mb)
+        {
+            mb.Entity<ApplicationResponse>().HasData(
+                new ApplicationResponse
+                {
+                    MovieId = 1,
+                    Category = "Drama",
+                    Title = "Emma",
+                    Year = 2020,
+                    Director = "Autumn de Wilde", 
+                    Rating = "PG"
+                },
+                new ApplicationResponse
+                {
+                    MovieId = 2,
+                    Category = "Drama",
+                    Title = "Pride and Prejudice",
+                    Year = 2005,
+                    Director = "Joe Wright",
+                    Rating = "PG"
+                },
+                new ApplicationResponse
+                {
+                    MovieId = 3,
+                    Category = "Family",
+                    Title = "Tangled",
+                    Year = 2010,
+                    Director = "Nathan Greno",
+                    Rating = "PG"
+                }
+            );
+        }
+    }
+}
